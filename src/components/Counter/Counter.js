a20c6693dbbc593e6f2b4bf4e531c8284c1c30c7
Kyle Bachand <kbachand@sbcglobal.net>
2023-10-26 12:26:03 -0500

    Instructor led: added a spinner to counter component when initializing.

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,20 +1,21 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner';
+
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    const savedValue = window.localStorage.getItem('saved-count'); 
-    if (savedValue === null){
-      return;
+    const savedValue = window.localStorage.getItem('saved-count');
+
+    setCount(savedValue ? Number(savedValue) : 0)
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof count === 'number') {
+      window.localStorage.setItem('saved-count', count);
     }
-
-    setCount(Number(savedValue));
-  }, [])
-
-  React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
   }, [count]);
 
   return (
@@ -22,7 +23,8 @@ function Counter() {
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
-      Count: {count}
+      Count:{' '}
+      {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
